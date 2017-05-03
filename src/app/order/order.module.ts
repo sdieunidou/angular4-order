4ec0365f5a0884884e33733bbb399648fe6643c4
Seb <s.dieunidou@gmail.com>
2017-05-03 11:29:23 +0200

    update input

--- src/app/order/order.module.ts
+++ src/app/order/order.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OrderRootComponent } from './order-root/order-root.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [OrderRootComponent],
   exports: [OrderRootComponent]
