6f07ec96f8d6a99109d8189c39f5765d17d6bfa8
Seb <s.dieunidou@gmail.com>
2017-05-03 15:48:04 +0200

    add form from catalog

--- src/app/order/order-catalog-form/order-catalog-form.component.ts
+++ src/app/order/order-catalog-form/order-catalog-form.component.ts
@@ -1,5 +1,8 @@
-import {Component, Injectable, OnInit} from '@angular/core';
+import {Component, EventEmitter, Injectable, OnInit, Output} from '@angular/core';
 import {CatalogService} from "../catalog.service";
+import {Observable} from "rxjs/Observable";
+import {Book} from "../book.model";
+import {OrderRow} from "../order-row.model";
 
 @Component({
   selector: 'app-order-catalog-form',
@@ -10,12 +13,33 @@ import {CatalogService} from "../catalog.service";
 
 @Injectable()
 export class OrderCatalogFormComponent implements OnInit {
-  books = this.catalog.getList();
+  books: Observable<Book[]>;
+
+  @Output() add = new EventEmitter<OrderRow>();
+
+  data: {
+    book: Book
+    quantity: number
+  };
 
   constructor(private catalog: CatalogService) {
 
   }
 
   ngOnInit() {
+      this.reset();
+      this.books = this.catalog.getList();
+  }
+
+  reset() {
+      this.data = {
+        book: <Book>{},
+        quantity: 1
+      }
+  }
+
+  submit(data) {
+    this.add.emit(new OrderRow(data.book.title, data.book.author, data.book.price, data.quantity));
+    this.reset();
   }
 }
